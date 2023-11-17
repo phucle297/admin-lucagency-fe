e6746e4663e43ab4f24896c3ed35f58d9e9c5408
Le Dam Duy Phuc <somdp2907@gmail.com>
2023-11-17 20:03:47 +0700

    feat: init customer detail

---
+++ src/pages/CustomerDetail/OrderDetail/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const OrderDetail: FC = () => {
+  return <div>OrderDetail</div>;
+};
+
+export default OrderDetail;
--- src/pages/CustomerDetail/index.tsx
+++ src/pages/CustomerDetail/index.tsx
@@ -1,9 +1,20 @@
 import { FC } from "react";
+import styles from "./index.module.scss";
+import { Col, Row } from "antd";
+import OrderDetail from "./OrderDetail";
+import CustomerInfo from "./CustomerInfo";
 
 const CustomerDetail: FC = () => {
   return (
-    <div>
-      <h1>CustomerDetail</h1>
+    <div className={styles.wrapper}>
+      <Row gutter={20}>
+        <Col xs={24} lg={18}>
+          <CustomerInfo />
+        </Col>
+        <Col xs={24} lg={6}>
+          <OrderDetail />
+        </Col>
+      </Row>
     </div>
   );
 };
